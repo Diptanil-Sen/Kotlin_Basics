29ca281baf8827abe806a7cd3c9c68101d1e7da8
Diptanil Sen <150945082+Diptanil-Sen@users.noreply.github.com>
2026-03-13 23:48:30 +0530

    Add coroutine builder example with parallel tasks
    Added a Kotlin coroutine example demonstrating coroutine builders and concurrent execution.
    Included:
    1.Launching coroutines using CoroutineScope.launch
    2.Creating multiple child coroutines (job1, job2) inside a parent coroutine
    3.Using suspend functions getData1() and getData2() to simulate asynchronous work with delay()
    4.Synchronizing completion using runBlocking and join()
    5.Logging thread names to illustrate coroutine execution
    
    This example demonstrates how coroutine builders enable concurrent task execution in Kotlin.

--- coroutines.kt
+++ coroutines.kt
@@ -84,3 +84,39 @@ import kotlinx.coroutines.Dispatchers.Main
 //
 //}
 //delay is a suspend function but it will not delayed in the same way so we are using her Run blocking Run blocking actually tells the function to wait for few seconds to complete the task and then finish the function here the main function will be start fake work will start and after few time of that 2000 seconds the fake function will end and main fuync willll end
+
+
+//<----------------------------------------------------------------->
+//coroutines builders
+
+//fun main(){
+//    println("Main program starts: ${Thread.currentThread().name}")
+//    val parentJob=CoroutineScope(Dispatchers.Default).launch {
+//        val job1=launch {
+//            val result1=getData1(Thread.currentThread().name)
+//            println(result1)
+//        }
+//        val job2=launch {
+//            val result2=getData2(Thread.currentThread().name)
+//            println(result2)
+//        }
+//    }
+//    runBlocking {
+//        parentJob.join()
+//    }
+//    println("Main program ends: ${Thread.currentThread().name}")
+//
+//}
+//private suspend fun getData1(threadName:String):String{
+//    println("Fake work1 starts:$threadName")
+//    delay(2000)
+//    println("Fake work1 finished:$threadName")
+//    return "Result 1"
+//}
+//
+//private suspend fun getData2(threadName:String):String{
+//    println("Fake work2 starts:$threadName")
+//    delay(2000)
+//    println("Fake work2 finished:$threadName")
+//    return "Result 2"
+//}
