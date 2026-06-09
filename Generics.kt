a5dffd675e4f2d6aa3362c0505aa9977d8b49bbf
Diptanil Sen <150945082+Diptanil-Sen@users.noreply.github.com>
2026-06-09 23:00:13 +0530

    Update Generics.kt

--- Generics.kt
+++ Generics.kt
@@ -10,6 +10,7 @@ fun main(args:Array<String>) {
    footballTeam.addPlayers(footballPlayer2)
 }
 
+
 class Team<T>(val name:String,val players: MutableList<T>){//<T> means This team can hold any one type of player. Example: Team<FootballPlayer> → football team ,Team<BaseballPlayer> → baseball team
    fun addPlayers(player : T){
        if(players.contains(player)){
